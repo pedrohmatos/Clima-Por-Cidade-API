188a7a01c1d2cc78326b00e1377fadebd50ec902
Pedro Matos <pedroheunapolis@gmail.com>
2023-08-19 18:42:35 -0300

    escrevendo o javascript da aplicação

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-/*
-    open weather API
- 0d456b92b1bbfaf3509d2c655e701a79 
-*/
+// Variáveis e seleção de elementos
+const apiWeatherKey = '0d456b92b1bbfaf3509d2c655e701a79';
+const apiFlag = 'https://flagsapi.com';
 
+const cidadeInput = document.querySelector('#inserir');
+const pesquisarBttn = document.querySelector('.pesquisar');
+
+const cidadeElemento = document.querySelector('#nomeDoLocal');
+const bandeiraElemento = document.querySelector('#bandeiraDoLocal');
+const tempElemento = document.querySelector('#grausTemp h1');
+const ceuElemento = document.querySelector('#ceu');
+const ceuIcone = document.querySelector('#ceuIcone');
+const umidadeElemento = document.querySelector('#umidade');
+const ventoElemento = document.querySelector('#vento');
+
+// Eventos
+pesquisarBttn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const cidade = cidadeInput.value;
+
+    mostrarClimaDados(cidade);
+});
+
+// Funcoes
+
+const mostrarClimaDados = (cidade) => {
+    console.log(cidade);
+};
+
+const pegarClimaDados = async (cidade) => {
+
+    const apiWeather = fetch();
+};
